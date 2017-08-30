dac125470b83c536da1f49621d7dec1e17bc47dc
qcrist <qcrist@gmail.com>
2017-08-30 15:35:15 -0400

    remove .gitignored files

--- client/scripts/init.js
+++
@@ -1,8 +0,0 @@
-//noinspection JSFileReferences
-require.config({
-    paths: {
-        jquery: "https://code.jquery.com/jquery-3.2.1.min.js"
-    }
-});
-requirejs(["scripts/main"]);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5pdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImluaXQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsK0JBQStCO0FBQy9CLE9BQU8sQ0FBQyxNQUFNLENBQUM7SUFDWCxLQUFLLEVBQUU7UUFDSCxNQUFNLEVBQUUsNkNBQTZDO0tBQ3hEO0NBQ0osQ0FBQyxDQUFDO0FBQ0gsU0FBUyxDQUFDLENBQUMsY0FBYyxDQUFDLENBQUMsQ0FBQyJ9
