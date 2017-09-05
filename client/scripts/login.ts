df73cbae2f9d8ed51aaff0d59f7f9b84d89050fd
Crist, Quintin Ray <qcrist@gatech.edu>
2017-09-05 12:58:54 -0400

    #3 #8 #9

---
+++ client/scripts/login.ts
@@ -0,0 +1,24 @@
+import * as $ from "jquery"
+
+namespace loginpage {
+
+    function do_login() {
+        //FIXME handle login
+        location.href = "main.html"; //FIXME URL
+        return false; //block form submit
+    }
+
+    function do_register() {
+        location.href = "register.html"; //FIXME URL
+        return false; //block form submit
+    }
+
+    function do_forgot() {
+        alert("stretch goal");
+    }
+
+    $("#login").click(do_login);
+    $("#register").click(do_register);
+    $("#forgot").click(do_forgot);
+    console.log("INIT");
+}
---
+++ client/scripts/requireconfig.ts
@@ -0,0 +1,5 @@
+requirejs.config({
+    paths: {
+        jquery: "https://code.jquery.com/jquery-3.2.1.min"
+    }
+});
