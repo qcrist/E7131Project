ea81dae10cf0c1d38d8ac0100091066503501838
george-tang <george.x.tang@gmail.com>
2017-11-10 11:54:15 -0500

    Add functionality for remove user from group

--- scripts/create_edit_group.ts
+++ scripts/create_edit_group.ts
@@ -6,3 +6,16 @@ $("#save").click(() => {
         return false;
     }
 });
+
+$(".remove_member").click(function() {
+    if (confirm("Remove " + $(this).prev().text() + " from your group?")) {
+        $(this).hide();
+        $(this).prev().hide();
+        $(this).prev().prev().hide();
+        submit_removal($(this).next().text());
+    }
+});
+
+let submit_removal = function(user_id: string) {
+    $.post("/action/remove_user_from_group", {id: user_id})
+}
